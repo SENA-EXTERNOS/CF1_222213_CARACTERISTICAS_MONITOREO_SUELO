64e346211482de62597d5a5bbe5195e89b87968b
elrilacho <camilobr17@gmail.com>
2023-03-30 11:21:46 -0500

    Componente listo

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -11,7 +11,7 @@
 
     figure.mb-5(data-aos="zoom-in")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/Op7Mt9I3lZQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
 
                   
